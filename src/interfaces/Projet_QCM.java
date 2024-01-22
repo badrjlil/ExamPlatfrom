73d83cfa2a3cdaebe97d871fe1de32fe4b1f98cb
Badr JLIL <badr.jlil@outlook.com>
2024-01-22 15:15:01 +0100

    interfaces added

---
+++ src/interfaces/Projet_QCM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_qcm;
+
+/**
+ *
+ * @author USER
+ */
+public class Projet_QCM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
